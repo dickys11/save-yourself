7026d17e22ab24162650b1d0228b13eca3dd912c
Muchammad Dicky Sanjaya <dicky.0201@gmail.com>
2021-05-30 08:47:40 +0700

    create end point to grab tweet using username

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import schemas
 import functions
 import json
 
+
 def getAPI():
     try:
         api = config.createAPI()
@@ -13,3 +14,22 @@ def getAPI():
     except Exception as e:
         print(f'[-] {e}')
 
+
+app = FastAPI()
+
+
+@app.get('/getstatus/{username}/{num_page}', response_model=schemas.Tweet)
+def getstatus(username: str, num_page: int, api = Depends(getAPI)):
+    tweet_list = []
+
+
+    for page in range(num_page):
+        user_timeline = api.user_timeline(username, page=page, tweet_mode="extended")
+        tweet_list += functions.makeList(user_timeline)
+    
+    results = {
+        'username': username,
+        'tweet': tweet_list
+    }
+
+    return results
