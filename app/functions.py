bdf73d1a38fccd94909ff2b634e0e401e4f119d9
Muchammad Dicky Sanjaya <dicky.0201@gmail.com>
2021-06-01 10:14:45 +0700

    lowercase the tweet before applying filter so the filter would work

--- app/functions.py
+++ app/functions.py
@@ -8,7 +8,7 @@ def makeList(user_timeline):
     tweet_list = []
     tweet_dict  = {}
     for tweet in user_timeline:
-        text = tweet._json['full_text']
+        text = tweet._json['full_text'].lower() #Lowercase
         if filterTweet(text):
             clean_text = cleanTweet(tweet._json['full_text'])
             print(f'[+] {clean_text}')
@@ -28,7 +28,6 @@ def cleanTweet(tweet):
     tweet = ''.join(c for c in tweet if c not in emoji.EMOJI_ALIAS_UNICODE_ENGLISH.values()) #Remove Emojis
     tweet = tweet.replace("#", "").replace("_", " ") #Remove hashtag sign but keep the text
     tweet = tweet.encode('ascii', 'ignore') #Remove non ascii character
-    tweet = tweet.lower() #Lowercase
 
     return tweet
 
